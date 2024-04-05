f52fa9fd5e4ce306cc3cae6bb739efb35879af5a
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-04-05 09:50:04 +0300

    fixture was added

--- tests/test_files_processing.py
+++ tests/test_files_processing.py
@@ -1,3 +1,21 @@
 import os
 import pytest
 from script import read_file, write_file, process_files
+
+
+@pytest.fixture
+def setup_files(request):
+    file1_path = "test_file1.txt"
+    file2_path = "test_file2.txt"
+
+    with open(file1_path, 'w') as file:
+        file.write(request.param[0])
+
+    with open(file2_path, 'w') as file:
+        file.write(request.param[1])
+
+    yield file1_path, file2_path
+
+    os.remove(file1_path)
+    os.remove(file2_path)
+
