07cbc00b78dbc7b7c5a74b671535d0b1486f8218
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-04-05 09:42:37 +0300

    process_files was updated and files for read was added

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
+import os
+
+
 def read_file(file_path):
     with open(file_path, "r") as file:
         return set(file.readlines())
@@ -8,13 +11,28 @@ def write_file(file_path, lines):
         file.writelines(lines)
 
 
-def process_files(first_file_path, second_file_path):
+def process_files(first_file_path, second_file_path, output_dir_path=""):
     file1 = read_file(first_file_path)
     file2 = read_file(second_file_path)
 
     diff_lines = file1.symmetric_difference(file2)
     same_lines = file1.intersection(file2)
 
-    write_file('diff.txt', sorted(diff_lines))
-    write_file('same.txt', sorted(same_lines))
+    if output_dir_path:
+        diff_output_path = os.path.join(output_dir_path, "diff.txt")
+        same_output_path = os.path.join(output_dir_path, "same.txt")
 
+        if not os.path.exists(output_dir_path):
+            os.makedirs(output_dir_path)
+    else:
+        diff_output_path = "diff.txt"
+        same_output_path = "same.txt"
+
+    write_file(diff_output_path, sorted(diff_lines))
+    write_file(same_output_path, sorted(same_lines))
+
+
+if __name__ == "__main__":
+    file1_path = os.path.join("files", "file1.txt")
+    file2_path = os.path.join("files", "file2.txt")
+    process_files(file1_path, file2_path)
