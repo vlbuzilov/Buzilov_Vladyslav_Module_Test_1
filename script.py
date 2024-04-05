846706d6f97b98bb5ae9db01cb2b77b9ddc768c8
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-04-05 09:29:53 +0300

    file processing was added

--- script.py
+++ script.py
@@ -6,3 +6,15 @@ def read_file(file_path):
 def write_file(file_path, lines):
     with open(file_path, "w") as file:
         file.writelines(lines)
+
+
+def process_files(first_file_path, second_file_path):
+    file1 = read_file(first_file_path)
+    file2 = read_file(second_file_path)
+
+    diff_lines = file1.symmetric_difference(file2)
+    same_lines = file1.intersection(file2)
+
+    write_file('diff.txt', sorted(diff_lines))
+    write_file('same.txt', sorted(same_lines))
+
